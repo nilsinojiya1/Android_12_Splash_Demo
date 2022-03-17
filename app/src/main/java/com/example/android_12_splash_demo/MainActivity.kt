4033b23384e39064138fe1d7a3849a190e70430d
mubarakansari799 <mubarak.ansari@brainvire.com>
2022-03-17 17:39:56 +0530

    Android Splash Screen

--- app/src/main/java/com/example/android_12_splash_demo/MainActivity.kt
+++ app/src/main/java/com/example/android_12_splash_demo/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.android_12_splash_demo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
-    var isUserLoggedIn = true
+    var isUserLoggedIn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashActivity = installSplashScreen()
@@ -16,13 +18,43 @@ class MainActivity : AppCompatActivity() {
          * setKeepOnScreenCondition pass true
          *
          */
-        splashActivity.setKeepOnScreenCondition { true }
-        if (isUserLoggedIn) {
-            startActivity(Intent(StartActivity@ this, LoginActivity::class.java))
-            finish()
-        } else {
-            startActivity(Intent(StartActivity@ this, DashboardActivity::class.java))
-            finish()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.d("MainActivity", "onCreate: I AM RUNNING ON API 12 or higher")
+            // custom exit on splashScreen
+
+            splashActivity.setKeepOnScreenCondition {
+                when(isUserLoggedIn){
+                    true -> {startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    }
+                    false ->{
+                        startActivity(Intent(this, DashboardActivity::class.java))
+                        finish()
+                    }
+                }
+                true
+            }
+
+
+            /*splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // custom animation.
+                ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_X,
+                    0f,
+                    splashScreenView.width.toFloat()
+                ).apply {
+                    duration = 1000
+                    // Call SplashScreenView.remove at the end of your custom animation.
+                    doOnEnd {
+                        splashScreenView.remove()
+                    }
+                }.also {
+                    // Run your animation.
+                    it.start()
+                }
+
+            }*/
         }
     }
 }
